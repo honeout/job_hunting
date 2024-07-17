b01d948d518c23c9301bb6e0f6c4fb2e96b1691c
honeout <143673256+honeout@users.noreply.github.com>
2024-07-17 16:25:37 +0900

    aaa

--- Source/Graphics/Graphics.cpp
+++ Source/Graphics/Graphics.cpp
@@ -201,6 +201,7 @@ Graphics::Graphics(HWND hWnd)
 
 	// ƒŒƒ“ƒ_ƒ‰
 	{
+
 		debugRenderer = std::make_unique<DebugRenderer>(device.Get());
 		lineRenderer = std::make_unique<LineRenderer>(device.Get(), 1024);
 		imguiRenderer = std::make_unique<ImGuiRenderer>(hWnd, device.Get());
